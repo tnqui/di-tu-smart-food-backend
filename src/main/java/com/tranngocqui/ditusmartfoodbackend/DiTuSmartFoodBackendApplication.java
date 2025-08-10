4e3dc6e2d86760fc8cada68485774b571728dde6
Ngoc Qui <125120853+quytranlx123@users.noreply.github.com>
2025-08-10 21:36:58 +0700

    init project

---
+++ src/main/java/com/tranngocqui/ditusmartfoodbackend/DiTuSmartFoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tranngocqui.ditusmartfoodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiTuSmartFoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiTuSmartFoodBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tranngocqui/ditusmartfoodbackend/DiTuSmartFoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranngocqui.ditusmartfoodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiTuSmartFoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
